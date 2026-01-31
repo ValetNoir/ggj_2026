40909e9e24a745ac0d164a221e5e6fc358e6a56b
Ghislain Thiery <ghislain.thiery@ntymail.com>
2026-01-31 21:43:28 +0100

    info label text + asset

--- scripts/info_flash.gd
+++ scripts/info_flash.gd
@@ -1,5 +1,22 @@
 extends Control
 
+@onready var info_flash_richtext: RichTextLabel = %info_flash_richtext
 
 func set_description(description: Description):
-	pass
+	var text: String
+	text += str("[ul]")
+	match description.color:
+		Description.MaskColor.RED:
+			text += str("This dino is red")
+		Description.MaskColor.GREEN:
+			text += str("This dino is green")
+		Description.MaskColor.BLUE:
+			text += str("This dino is blue")
+		_:
+			pass
+	text += str("[/ul]")
+	update_text(text)
+
+func update_text(text: String):
+	info_flash_richtext.clear()
+	info_flash_richtext.append_text(text)
