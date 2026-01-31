4f65de21545f0e7e503cf281b05be70791e357ca
valet noir <valetnoir.gaming@gmail.com>
2026-01-31 14:41:29 +0100

    modified phenotype names

--- scripts/phenotype.gd
+++ scripts/phenotype.gd
@@ -7,11 +7,10 @@ extends Resource
 @export var horns: bool
 @export var crest: bool
 
-@export var SILHOUTTE_TEXTURES: CompressedTexture2D = null
-@export var FACE_TEXTURE: CompressedTexture2D = null
-@export var HORNS_TEXTURES: CompressedTexture2D = null
+@export var SILHOUTTE_TEXTURE: CompressedTexture2D = null
+@export var MOUTH_TEXTURE: CompressedTexture2D = null
+@export var HORNS_TEXTURE: CompressedTexture2D = null
 @export var EMOTIONS_TEXTURES: Dictionary[Description.Emotion, CompressedTexture2D] = {}
-static var EYES_TEXTURE: CompressedTexture2D = preload("uid://d3v8eynat77lb")
 
 func _init():
 	species = Description.Species.DIPLODOCUS
