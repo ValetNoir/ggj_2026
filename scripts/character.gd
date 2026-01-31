0baa78c558197ab45da7a1bab258334c7e7046f2
Aeropulu <42248174+Aeropulu@users.noreply.github.com>
2026-01-31 14:40:00 +0100

    hold character with mouse hover

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,7 +1,8 @@
+class_name Character
 extends Node2D
 
 @onready var _area: Area2D = $Area2D
-
+static var _held_character: Character = null
 @export var speed: float = 100.0
 var _destination: Vector2 = Vector2.ZERO
 var _is_moving: bool = false
@@ -12,12 +13,16 @@ const MIN_DESTINATION_DISTANCE: float = 100.0
 const MAX_DESTINATION_DISTANCE: float = 200.0
 const MIN_START_DELAY: float = 0.1
 const MAX_START_DELAY: float = 1.5
+const NORMAL_Z_INDEX: int = 0
+const HELD_Z_INDEX: int = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_valid_rect = get_viewport_rect().grow(-30.0)
 	get_tree().create_timer(randf_range(MIN_START_DELAY, MAX_START_DELAY))\
 		.timeout.connect(_start_move)
+	_area.mouse_entered.connect(_on_area_mouse_enter)
+	_area.mouse_exited.connect(_on_area_mouse_exit)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -25,6 +30,8 @@ func _process(delta: float) -> void:
 		_move(delta)
 
 func _move(delta: float) -> void:
+	if _held_character == self:
+		return
 	global_position = global_position.move_toward(_destination, speed * delta)
 	if global_position.is_equal_approx(_destination):
 		_is_moving = false
@@ -35,7 +42,6 @@ func _start_move() -> void:
 	var direction: Vector2 = _get_move_direction()
 	_destination = global_position + distance * direction 
 	_destination = _reflect_destination(_destination)
-	print("%s move to %s", [name, _destination])
 	_is_moving = true
 
 func _reflect_destination(destination: Vector2) -> Vector2:
@@ -81,6 +87,36 @@ func _set_timer_to_next_move() -> void:
 	var timer_duration = randf_range(MIN_TIMER_DURATION, MAX_TIMER_DURATION)
 	var timer := get_tree().create_timer(timer_duration)
 	timer.timeout.connect(_start_move)
+
+func _on_area_mouse_enter() -> void:
+	if not is_instance_valid(_held_character):
+		z_index = HELD_Z_INDEX
+		_held_character = self
+
+func _on_area_mouse_exit() -> void:
+	if _held_character == self:
+		z_index = NORMAL_Z_INDEX
+		_hold_next_character()
+
+func _hold_next_character() -> void:
+	var mouse_position = get_viewport().get_mouse_position()
+	var query := PhysicsPointQueryParameters2D.new()
+	query.position = mouse_position
+	query.collide_with_areas = true
+	var intersects := get_world_2d().direct_space_state.intersect_point(query)
+		
+	for result:Dictionary in intersects:
+		if not result.has("collider"):
+			continue
+		var collider: Node2D = result.collider
+		if collider.get_parent() is Character:
+			_held_character = collider.get_parent()
+			_held_character.z_index = HELD_Z_INDEX
+			return
+	# default case if no valid hit
+	_held_character = null
+	return
+
 
 func _on_area_2d_input_event(viewport: Viewport, event: InputEvent, shape_idx: int) ->void:
 	if event is InputEventMouseButton and event.is_pressed():
