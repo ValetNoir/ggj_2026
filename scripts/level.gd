09175bf026afc8e55cb1aaabd12399d805db00a3
Aeropulu <42248174+Aeropulu@users.noreply.github.com>
2026-01-31 18:47:46 +0100

    level resources

---
+++ scripts/level.gd
@@ -0,0 +1,7 @@
+class_name Level
+extends Resource
+
+@export var npc_count: int = 7
+@export var max_similarity: int = 0
+@export var clue_duration: float = 5.0
+@export var gain_life: bool = false
